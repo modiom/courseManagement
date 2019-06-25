4fcc1b184ea3c4268def697b62425d239225655b
DhruthiAccolite <46513359+DhruthiAccolite@users.noreply.github.com>
2019-06-25 04:18:22 +0000

    SQL file for course table create queries

---
+++ CourseSQLFIle.sql
@@ -0,0 +1,89 @@
+create database course;
+use course;
+
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema COURSE
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema COURSE
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `COURSE` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema course
+-- -----------------------------------------------------
+USE `COURSE` ;
+
+-- -----------------------------------------------------
+-- Table `COURSE`.`course`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `COURSE`.`course` (
+  `course_id` INT NOT NULL,
+  `course_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`course_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `COURSE`.`trainer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `COURSE`.`trainer` (
+  `trainer_id` INT NOT NULL,
+  `trainer_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`trainer_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `COURSE`.`course_trainer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `COURSE`.`course_trainer` (
+  `course_id` INT NOT NULL,
+  `trainer_id` INT NOT NULL,
+  PRIMARY KEY (`course_id`, `trainer_id`),
+  INDEX `fk_course_trainer_trainer1_idx` (`trainer_id` ASC) VISIBLE,
+  CONSTRAINT `fk_course_trainer_course`
+    FOREIGN KEY (`course_id`)
+    REFERENCES `COURSE`.`course` (`course_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_course_trainer_trainer1`
+    FOREIGN KEY (`trainer_id`)
+    REFERENCES `COURSE`.`trainer` (`trainer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `COURSE`.`trainer_allocation`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `COURSE`.`trainer_allocation` (
+  `start_time` DATETIME NULL,
+  `end_time` DATETIME NULL,
+  `course_id` INT NOT NULL,
+  `trainer_id` INT NOT NULL,
+  PRIMARY KEY (`course_id`, `trainer_id`),
+  INDEX `fk_trainer_allocation_trainer1_idx` (`trainer_id` ASC) VISIBLE,
+  CONSTRAINT `fk_train_allocation_course1`
+    FOREIGN KEY (`course_id`)
+    REFERENCES `COURSE`.`course` (`course_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_trainer_allocation_trainer1`
+    FOREIGN KEY (`trainer_id`)
+    REFERENCES `COURSE`.`trainer` (`trainer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
